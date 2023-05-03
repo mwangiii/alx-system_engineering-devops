190bbc0bc519adbe7bb2c5377208844f12ea927c
mwangiii <mwangi8kevin@gmail.com>
2023-05-04 00:32:37 +0300

    rb regex

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 
-# accepts one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/hb{2,5}n/).join
+# Prints all the words that start with h, end with n, and have between 2 and 5 t's in between.
+puts ARGV[0].scan(/hbt{2,5}n/).join
