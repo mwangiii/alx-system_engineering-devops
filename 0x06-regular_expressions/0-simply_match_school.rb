72711effb3597d3be64e755ef2722e0284c239d1
mwangiii <mwangi8kevin@gmail.com>
2023-05-04 00:08:40 +0300

    rb regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 
+# Get the first command line argument
+argument = ARGV[0]
+
+# Define a regular expression to match "School"
 regex = /School/
+
+# Use the regular expression to match the argument
+if regex.match(argument)
+  puts argument + "$"
+else
+  puts ""
+end
