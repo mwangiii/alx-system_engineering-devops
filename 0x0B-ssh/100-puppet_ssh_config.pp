4f1a12f96ce871823cdbaacad233af14a1dfd45b
mwangiii <mwangi8kevin@gmail.com>
2023-07-13 17:43:56 +0300

    changing adv file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,40 @@
-file { '/etc/ssh/sshd_config':
-  ensure  => file,
-  content => epp('ssh/sshd_config.epp'),
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => epp('ssh/ssh_config.epp'),
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-exec { 'reload_sshd_config':
-  command     => '/usr/sbin/service sshd reload',
-  refreshonly => true,
-  subscribe   => File['/etc/ssh/sshd_config'],
-}
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
